f2786c943d55e8a6a43d6ce9678a1b82e6f5ea2a
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 23:07:54 +0100

    Update vnc_startup.sh

--- startup/vnc_startup.sh
+++ startup/vnc_startup.sh
@@ -29,6 +29,7 @@ if [[ $1 =~ -h|--help ]]; then
 fi
 
 # should also source $STARTUPDIR/generate_container_user
+echo 'source $STARTUPDIR/generate_container_user' >> $HOME/.bashrc
 source $HOME/.bashrc
 
 # add `--skip` to startup args, to skip the VNC startup procedure
