4ade54bcb5e390bcb499ab5a37c8f142631bc650
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 15:12:24 +0100

    Update desktopgui.sh

--- install/desktopgui.sh
+++ install/desktopgui.sh
@@ -7,10 +7,6 @@ echo "Install default Ubuntu Desktop"
 ### Install default ubuntu desktop experience
 apt-get update 
 apt-get install -y --no-install-recommends ubuntu-desktop
-# tasksel install ubuntu-desktop
-# tasksel install ubuntu-desktop --no-install-recommends
 
 ### Start GNOME session?
-service gdm3 start
-#systemctl enable gdm
-#systemctl start gdm
+#service gdm3 start
