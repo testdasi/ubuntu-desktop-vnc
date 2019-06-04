a4e663040a74c3512fde95309cf83f8974c0a335
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 13:22:19 +0100

    Update prerequisite.sh

--- install/prerequisite.sh
+++ install/prerequisite.sh
@@ -7,5 +7,5 @@ apt-get update
 apt-get install -y vim wget net-tools locales bzip2 python-numpy mc tasksel libnss-wrapper gettext
 apt-get clean -y
 
-echo "Generate locales for en_GB.UTF-8 because I'm British"
+echo "Generate locales for en_GB.UTF-8 because I'm posh like that"
 locale-gen en_GB.UTF-8
