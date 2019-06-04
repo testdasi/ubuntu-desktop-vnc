82cb06560fb2b56148edc6928b7992855394f596
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 12:45:50 +0100

    Create desktopgui.sh

---
+++ install/desktopgui.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+### Install default ubuntu desktop experience
+tasksel install ubuntu-desktop
+
+### Start GNOME session?
+#service gdm3 start
+systemctl enable gdm3
+systemctl start gdm3
