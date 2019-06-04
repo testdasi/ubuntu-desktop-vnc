2ba852ec9d3d9ef4e8e25e6fe8b9b9f6a7c9a3eb
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 13:21:23 +0100

    Update postsscript.sh

--- install/postsscript.sh
+++ install/postsscript.sh
@@ -6,3 +6,12 @@ set -u
 echo "add 'source generate_container_user' to .bashrc"
 # have to be added to hold all env vars correctly
 echo 'source $STARTUPDIR/generate_container_user' >> $HOME/.bashrc
+
+echo "Apt Cleanup"
+RUN apt-get update -y
+RUN apt-get upgrade -y
+RUN apt-get dist-upgrade -y
+RUN apt-get clean -y
+RUN apt-get autoclean -y
+RUN apt-get autoremove -y
+
