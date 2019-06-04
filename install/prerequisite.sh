4f64dbfaa21009df5667e5ff409c523754727abe
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 14:30:29 +0100

    Update prerequisite.sh

--- install/prerequisite.sh
+++ install/prerequisite.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "Install some common tools for further installation"
-apt-get update
+apt-get update 
+apt-get install -y apt-utils
 apt-get install -y vim wget net-tools locales bzip2 python-numpy mc tasksel libnss-wrapper gettext
 apt-get clean -y
 
