486a958d557c612213328e57b8bc8ff32ec56459
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 13:04:06 +0100

    Update prerequisite.sh

--- install/prerequisite.sh
+++ install/prerequisite.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 python-numpy mc tasksel
+apt-get install -y vim wget net-tools locales bzip2 python-numpy mc tasksel libnss-wrapper gettext
 apt-get clean -y
 
 echo "Generate locales for en_GB.UTF-8 because I'm British"
