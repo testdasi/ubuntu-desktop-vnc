2c4950ed8ad65489b46316c8366277fd51b87adf
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-04 12:37:37 +0100

    Update prerequisite.sh

--- install/prerequisite.sh
+++ install/prerequisite.sh
@@ -4,9 +4,8 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 \
-    python-numpy #used for websockify/novnc
+apt-get install -y vim wget net-tools locales bzip2 python-numpy mc tasksel
 apt-get clean -y
 
-echo "generate locales für en_US.UTF-8"
-locale-gen en_US.UTF-8
+echo "Generate locales for en_GB.UTF-8 because I'm British"
+locale-gen en_GB.UTF-8
